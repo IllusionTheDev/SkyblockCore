67d57ddfaf166c7c50299dc43c6414412a1f75b2
Illusion <imdatillusion@gmail.com>
2021-01-13 15:57:43 +0000

    Added missing classes

---
+++ src/main/java/me/illusion/utilities/LocationUtil.java
@@ -0,0 +1,24 @@
+package me.illusion.utilities;
+
+import org.bukkit.Location;
+
+public final class LocationUtil {
+
+    private LocationUtil() {
+        // Empty private constructor for static Utility class
+    }
+
+    /**
+     * Checks if a location is within a cuboid area made by 2 locations
+     *
+     * @param location    - The location to compare
+     * @param topLeft     - The top left point of the cuboid
+     * @param bottomRight - The bottom right point of the cuboid
+     * @return TRUE if location is inside, FALSE otherwise
+     */
+    public static boolean locationBelongs(Location location, Location topLeft, Location bottomRight) {
+        return !(location.getBlockY() > topLeft.getBlockY() || location.getBlockY() < bottomRight.getBlockY() || //compare y
+                location.getBlockX() < topLeft.getBlockX() || location.getBlockX() > bottomRight.getBlockX() || //compare x
+                location.getBlockZ() < topLeft.getBlockZ() || location.getBlockZ() > bottomRight.getBlockZ()); //compare z
+    }
+}
